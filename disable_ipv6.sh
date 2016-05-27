cf0d3993aac20a59caa749cb8e0ae9ba4906dc63
Joao Marcelo Barros <joaomarcelobarros@gmail.com>
2016-05-26 22:58:27 -0300

    disable ipv6

--- disable_ipv6.sh
+++ disable_ipv6.sh
@@ -4,4 +4,6 @@ echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
 
-sysctl -p cat /proc/sys/net/ipv6/conf/all/disable_ipv6
+sysctl -p 
+
+cat /proc/sys/net/ipv6/conf/all/disable_ipv6
